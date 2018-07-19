01c17b839c01a12cdcf19b5f42dda34e807bc98f
Matt D. Kearns <mattdkearns@gmail.com>
2018-07-18 23:14:01 -0400

    add about page with button link on home

--- app.py
+++ app.py
@@ -5,13 +5,20 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    """Index.html"""
-    #return 'Hello, world!'
+    """index.html"""
     return open('./index.html', 'r').read()
+
+@app.route('/about.html')
+def about():
+    """about.html"""
+    return open('./about.html', 'r').read()
+
 
 if __name__ == '__main__':
     app.run()
